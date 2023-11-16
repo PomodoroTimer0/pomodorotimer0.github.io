61110d103bb72ec9427078cd8e531b158da4e16a
PomodoroTimer0 <150753382+PomodoroTimer0@users.noreply.github.com>
2023-11-17 00:10:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -249,5 +249,5 @@ function deleteEntries() {
 }
 function changeVideo(videoUrl) {
     const youtubePlayer = document.getElementById('youtubePlayer');
-    youtubePlayer.src = 'data:text/html;charset=utf-8,' + encodeURIComponent('<iframe width="660" height="415" src="' + videoUrl + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
+    youtubePlayer.innerHTML = '<iframe width="560" height="315" src="' + videoUrl + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
 }
